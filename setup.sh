d69983ac4afa59aebbc9196ec85555996fc6876b
p978 <59633184+p978@users.noreply.github.com>
2022-09-25 11:16:33 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,3 +6,8 @@ nvim
 cp -r wallpaper ~/.config/i3/wallpaper 
 cp .zshrc ~/.zshrc
 
+git clone --depth=1 https://github.com/adi1090x/polybar-themes.git
+cd polybar-themes
+chmod +x setup.sh
+./setup.sh
+
